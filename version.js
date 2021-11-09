ad51532b704f5f34855532cbc1d6585481029a01
gerardo-garcia19 <93272224+gerardo-garcia19@users.noreply.github.com>
2021-11-09 07:40:52 -0600

    Update version.js

--- version.js
+++ version.js
@@ -1,1 +1,1 @@
-aws lambda create-alias --function-name PruebaABC --name Dev --function-version 1 --description "Alias"
+aws lambda create-alias --function-name PruebaABC --name Develop --function-version 1 --description "Desarrollo"
