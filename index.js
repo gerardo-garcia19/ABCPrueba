37a99622a0aa3d244a59ad341d8706c4817adc06
Magaly <magaly.carro@iteraprocess.com>
2021-11-17 21:56:29 +0000

    Cambio

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Cambio en lambda version8'),
+        body: JSON.stringify('Cambio en lambda version9'),
     };
     return response;
 };
