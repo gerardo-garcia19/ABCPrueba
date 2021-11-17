b2a5d1701ff6a3919e49a7d4bcd9e593e056537f
EC2 Default User <ec2-user@ip-172-31-87-89.ec2.internal>
2021-11-17 02:59:14 +0000

    Cambio

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Cambio en lambda version4'),
+        body: JSON.stringify('Cambio en lambda version7'),
     };
     return response;
 };
